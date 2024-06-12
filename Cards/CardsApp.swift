4f2395f1989bf9e264beb333acbcb299cb107a4a
romashkarrr <r0man.kish999@gmail.com>
2024-06-12 11:17:08 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by romashka on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
